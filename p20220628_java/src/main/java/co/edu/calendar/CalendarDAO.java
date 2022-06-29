1b85dedc4dd436474194827dabaf9ce384cca288
leeheesu <h2su0712@naver.com>
2022-06-29 16:17:03 +0900

    fullcalendar, jquery

--- p20220628_java/src/main/java/co/edu/calendar/CalendarDAO.java
+++ p20220628_java/src/main/java/co/edu/calendar/CalendarDAO.java
@@ -7,17 +7,67 @@ import java.util.List;
 import co.edu.DAO;
 
 public class CalendarDAO extends DAO {
-	
-	public List<CalendarVO> getSchedule(){
+
+	// 입력
+
+	public boolean insertSchedule(CalendarVO vo) {
+
+		// 정상적으로 한 건 입력이 되면 true를,
+		// 예외가 발생하거나 입력이 안 되면 false를 리턴
+
+		getConnect();
+		String sql = "insert into full_calendar values(?,?,?)";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, vo.getTitle());
+			psmt.setString(2, vo.getStartDate());
+			psmt.setString(3, vo.getEndDate());
+
+			int cnt = psmt.executeUpdate();
+			if (cnt > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
+	}
+
+	public boolean deleteSchedule(String delTitle) {
+
+		// 정상적으로 한 건 입력이 되면 true를,
+		// 예외가 발생하거나 입력이 안 되면 false를 리턴
+
+		getConnect();
+		String sql = "delete full_calendar where title=?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, delTitle);
+
+			int cnt = psmt.executeUpdate();
+			if (cnt > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
+	}
+
+	public List<CalendarVO> getSchedule() {
 		// 전체 일정 정보를 가지고 오도록 메소드 완성
 		getConnect();
 		List<CalendarVO> list = new ArrayList<>();
 		String sql = "select * from full_calendar";
-		
+
 		try {
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				CalendarVO vo = new CalendarVO();
 				vo.setTitle(rs.getString("title"));
 				vo.setStartDate(rs.getString("start_date"));
@@ -29,9 +79,8 @@ public class CalendarDAO extends DAO {
 		} finally {
 			disconnect();
 		}
-		
+
 		return list;
 	}
 
-	
 }
--- p20220628_java/src/main/java/co/edu/calendar/FullCalendarServlet.java
+++ p20220628_java/src/main/java/co/edu/calendar/FullCalendarServlet.java
@@ -1,6 +1,7 @@
 package co.edu.calendar;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -39,7 +40,7 @@ public class FullCalendarServlet extends HttpServlet {
 
 		CalendarDAO dao = new CalendarDAO();
 		List<CalendarVO> list = dao.getSchedule();
-		
+
 		Gson gson = new GsonBuilder().create();
 		response.getWriter().print(gson.toJson(list));
 	}
@@ -50,6 +51,42 @@ public class FullCalendarServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/json;charset=utf-8");
+		Gson gson = new GsonBuilder().create();
+		PrintWriter out = response.getWriter();
+		// 파라미터 정보: cmd=insert, title=입력값, start=입력값, end=입력값
+
+		String cmd = request.getParameter("cmd");
+		String title = request.getParameter("title");
+		String start = request.getParameter("start");
+		String end = request.getParameter("end");
+
+		CalendarVO vo = new CalendarVO();
+		vo.setTitle(title);
+		vo.setStartDate(start);
+		vo.setEndDate(end);
+
+		CalendarDAO dao = new CalendarDAO();
+
+		if (cmd.equals("insert")) {
+			// vo 생성이랑 setter를 여기?
+			if (dao.insertSchedule(vo)) {
+				// {"retCode" : "Success"}
+				out.print("{\"retCode\" : \"Success\"}");
+			} else {
+				out.print("{\"retCode\" : \"Fail\"}");
+			}
+		} else if (cmd.equals("del")) {
+			String delTitle = request.getParameter("title");
+			if (dao.deleteSchedule(delTitle)) {
+				// {"retCode" : "Success"}
+				out.print("{\"retCode\" : \"Success\"}");
+			} else {
+				out.print("{\"retCode\" : \"Fail\"}");
+			}
+		}
 	}
 
 }
